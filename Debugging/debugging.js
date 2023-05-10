b854cedf309cdc6693cfe82fc1181fcd9d521bce
Zeynep Akkaya <132295746+zeynep-1@users.noreply.github.com>
2023-05-10 13:05:09 +0300

    4 - Debugging

---
+++ Debugging/debugging.js
@@ -0,0 +1,24 @@
+/*syntax errors that prevent your program from running,
+runtime errors where your code has unexpected behavior,
+logical errors where your code doesn't do what you intended
+*/
+
+console.clear();
+
+console.log(typeof []); // object
+console.log(typeof {}); // object
+
+/*
+JavaScript recognizes seven primitive (immutable) data types:
+Boolean, Null, Undefined, Number, String, Symbol (new with ES6),
+BigInt (new with ES2020), and one type for mutable items: Object.
+*/
+
+// reference error -> Misspelling
+
+/*
+"falsy" values
+false, 0, "", NaN, undefined, null
+*/
+
+// Off by one errors OBOE
